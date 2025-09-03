20b3d8cd646614528c463ef8bbd22685cd174386
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-09-03 23:11:19 +0300

    Написано перечисление для типов токена

---
+++ obfuscation_program/inc/tokenizer/token_type.hpp
@@ -0,0 +1,23 @@
+#ifndef TOKEN_TYPE_HPP
+#define TOKEN_TYPE_HPP
+
+
+enum token_type {
+    unknown_,
+    literal_,
+    key_word_,
+    data_type,
+    identifier_,
+    operator_,
+    semicolon_,
+    comment_,
+    multi_line_comment_,
+    preprocessor_directive_,
+    openning_parenthesis_,
+    closing_parenthesis_,
+    openning_curly_brace_,
+    closing_curly_brace_,
+};
+
+
+#endif // TOKEN_TYPE_HPP
