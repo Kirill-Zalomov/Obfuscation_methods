2ffe3466212510d52faa63c060f149f8258af475
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-09-04 23:21:53 +0300

    Написана реализация парсера аргументов утилиты

---
+++ obfuscation_program/src/args/args_parser.cpp
@@ -0,0 +1,83 @@
+#include "args_parser.hpp"
+
+
+bool args_parser::check_opt_delete_newlines(const std::list<std::string> args) const
+{
+    return std::find(args.begin(), args.end(), "--delete-newlines") != args.end();
+}
+
+
+bool args_parser::check_opt_add_garbage_lines(const std::list<std::string> args) const
+{
+    return std::find(args.begin(), args.end(), "--add-garbage-lines") != args.end();
+}
+
+
+bool args_parser::check_opt_add_random_spaces(const std::list<std::string> args) const
+{
+    return std::find(args.begin(), args.end(), "--add-random_spaces") != args.end();
+}
+
+
+bool args_parser::check_opt_mess_up_comments(const std::list<std::string> args) const
+{
+    return std::find(args.begin(), args.end(), "--mess-up-comment") != args.end();
+}
+
+
+bool args_parser::check_opt_rename_variables(const std::list<std::string> args) const
+{
+    return std::find(args.begin(), args.end(), "--rename-variables") != args.end();
+}
+
+
+bool args_parser::check_opt_in(const std::list<std::string> args) const
+{
+    for (const auto& arg : args)
+    {
+        if (arg.find("--in=") == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool args_parser::check_opt_out(const std::list<std::string> args) const
+{
+    for (const auto& arg : args)
+    {
+        if (arg.find("--out=") == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+std::string args_parser::get_path_to_input_file(const std::list<std::string> args)
+{
+    for (const auto& arg : args)
+    {
+        if (arg.find("--in=") == 0)
+        {
+            return arg.substr(5); // Получаем путь после "--in="
+        }
+    }
+    throw std::runtime_error("Input file option (--in) not found");
+}
+
+
+std::string args_parser::get_path_to_output_file(const std::list<std::string> args)
+{
+    for (const auto& arg : args)
+    {
+        if (arg.find("--out=") == 0)
+        {
+            return arg.substr(6); // Получаем путь после "--out="
+        }
+    }
+    throw std::runtime_error("Output file option (--out) not found");
+}
