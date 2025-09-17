47111e1d2f4722ca127607be85556dc46a065ed7
Kirill Zalomov <kirill.zalomov@gmail.com>
2025-09-17 14:25:13 +0300

    Первая лабораторная перенесена в папку lab1

---
+++ lab1/Obfuscation_program/build/CMakeFiles/obfuscation_program.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obfuscation_program.dir/link.d"
+  "CMakeFiles/obfuscation_program.dir/main.cpp.o"
+  "CMakeFiles/obfuscation_program.dir/main.cpp.o.d"
+  "CMakeFiles/obfuscation_program.dir/obfuscator.cpp.o"
+  "CMakeFiles/obfuscation_program.dir/obfuscator.cpp.o.d"
+  "obfuscation_program"
+  "obfuscation_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obfuscation_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
